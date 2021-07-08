cfb1149f56f77ef1d3517d1f817ece54a11a527c
AbdallahElsaeed761 <77922312+AbdallahElsaeed761@users.noreply.github.com>
2021-07-08 02:14:02 +0200

    Creating Data base in mongo and Add controller with create action and index.

---
+++ BookStoreInMongo/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using BookStoreInMongo.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStoreInMongo.Controllers
+{
+    public class BookController : Controller
+    {
+        MongoClient client = new MongoClient("mongodb://127.0.0.1:27017/");
+       // GET: List Of Book
+        public ActionResult Index()
+        {
+            var database = client.GetDatabase("BookStoreInventory");
+            var table = database.GetCollection<Book>("books");
+            var books = table.Find(FilterDefinition<Book>.Empty).ToList();
+            return View(books);
+        }
+        public ActionResult Create() => View();
+       
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            var database = client.GetDatabase("BookStoreInventory");
+            var table = database.GetCollection<Book>("books");
+          //  book._id = Guid.NewGuid().ToString();
+            table.InsertOne(book);
+           // ViewBag.save = "created New Book Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookStoreInMongo/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace BookStoreInMongo.Models
+{
+    public class Book
+    {
+        //[BsonRepresentation(BsonType.ObjectId)]
+        //public string _id { get; set; }
+        [XmlIgnore]
+        public ObjectId _id { get; set; }
+
+        public string MongoId
+        {
+            get { return _id.ToString(); }
+            set { _id = ObjectId.Parse(value); }
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
